7ecee4a7d50e57f4d670c1072a8b3c23c2629402
Angelo Mancilla Mendoza <angelo.mancilla.dev@gmail.com>
2026-01-19 18:04:22 -0500

    feat: :sparkles: Project structure initialized, dependencies and Docker setup ready

---
+++ src/main/java/com/_9/inspect_pro/InspectProApplication.java
@@ -0,0 +1,13 @@
+package com._9.inspect_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_9/inspect_pro/InspectProApplicationTests.java
@@ -0,0 +1,13 @@
+package com._9.inspect_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
